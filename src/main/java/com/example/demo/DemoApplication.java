fc774406c3e7a74042ebabdc02034c53863bc456
hbk <540243427@qq.com>
2018-04-02 23:37:57 +0800

    potimize login.html and add druid monitor

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.example")
 @EntityScan("com.example.repository.entity")
 @MapperScan("com.example.repository.mapper")
+@ServletComponentScan("com.example")
 public class DemoApplication {
 
 	public static void main(String[] args) {
